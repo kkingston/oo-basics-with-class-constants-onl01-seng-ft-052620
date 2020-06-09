fd7d62f5623406a271f9eefe2e6496efc55d7a04
Kayla Kingston <kkingston09@gmail.com>
2020-06-09 12:23:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,17 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
   
   GENRE = []
 
   def initialize(title)
     @title = title
   end
+  
+  def genre=(genre)
+    @genre = genre
+    GENRE << genre
+  end 
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
